6c17c43842b3a7c06b496c143e0e6f74b557a552
fengfan1 <554708195@qq.com>
2017-07-30 16:20:10 +0800

    Initial commit

---
+++ src/main/java/InterruptTest.java
@@ -0,0 +1,49 @@
+import java.security.Provider;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by F-victor on 2017/7/30.
+ */
+public class InterruptTest {
+
+    public static void main(String[] args){
+        MyService s = new MyService();
+        Thread t1 = new MyThread(s);
+        Thread t2 = new MyThread(s);
+        t1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        t2.start();
+        t2.interrupt();
+    }
+}
+
+class MyService{
+    ReentrantLock lock = new ReentrantLock();
+    public void service(){
+        try {
+            System.out.println(Thread.currentThread().getName() + " is trying to get lock");
+            //lock.lockInterruptibly();
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " is enjoying its service");
+            while(true);
+        } catch (Exception e) {
+            System.out.println(Thread.currentThread().getName() + " receive interrupt");
+        }
+
+    }
+}
+class MyThread extends Thread{
+    MyService s;
+    public MyThread(MyService s){
+        this.s = s;
+    }
+    @Override
+    public void run() {
+        s.service();
+    }
+}
